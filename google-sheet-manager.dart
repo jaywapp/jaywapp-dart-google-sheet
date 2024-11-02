63d505e6e642a6be1b8b190bcdb1ede030c7331c
unknown <jaywapp16@gmail.com>
2024-11-02 11:34:10 +0900

    Update 함수 추가

--- google-sheet-manager.dart
+++ google-sheet-manager.dart
@@ -1,9 +1,5 @@
 import 'dart:ffi';
-
 import 'package:be_my_colleague/Service/GoogleHttpClient.dart';
-import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:googleapis/sheets/v4.dart' as sheets;
 import 'package:google_sign_in/google_sign_in.dart';
 import 'google-sheet-range.dart';
@@ -18,8 +14,7 @@ class GoogleSheetManager {
   }
 
   Future<List<List<Object?>>?> GetActiveValues(String sheetName) async {
-    if (account == null) 
-     return List.empty();
+    if (account == null) return List.empty();
 
     final headers = await account?.authHeaders ?? new Map<String, String>();
     final authenticatedClient = GoogleHttpClient(headers);
@@ -35,7 +30,8 @@ class GoogleSheetManager {
       final int rowCount = sheet.properties!.gridProperties!.rowCount!;
       final int colCount = sheet.properties!.gridProperties!.columnCount!;
 
-      final range = GoogleSheetRange.Create(sheetName, 1, 1, colCount, rowCount);
+      final range =
+          GoogleSheetRange.Create(sheetName, 1, 1, colCount, rowCount);
       final rangeStr = range.toString();
 
       var response = await sheets.values.get(sheetID, rangeStr);
@@ -63,9 +59,33 @@ class GoogleSheetManager {
     return result;
   }
 
+  Future<void> Update(String sheetName, int index, List<Object?> row) async {
+    final headers = await account?.authHeaders ?? new Map<String, String>();
+    final authenticatedClient = GoogleHttpClient(headers);
+    final sheetsApi = sheets.SheetsApi(authenticatedClient);
+    try {
+      final spreadsheet = await sheetsApi.spreadsheets.get(sheetID);
+      final sheet = spreadsheet.sheets!
+          .firstWhere((sheet) => sheet.properties!.title == sheetName);
+      final int colCount = sheet.properties!.gridProperties!.columnCount!;
+
+      var range = GoogleSheetRange.Create(sheetName, 1, index, colCount, index)
+          .toString();
+
+      List<List<Object?>> values = [];
+      values.add(row);
+
+      var valueRange = sheets.ValueRange(
+        range: range,
+        values: values,
+      );
+      await sheetsApi.spreadsheets.values
+          .update(valueRange, sheetID, range, valueInputOption: 'RAW');
+    } catch (e) {}
+  }
+
   Future<List<List<Object?>>?> GetRowsByCellValue(
       String sheetName, int columnIndex, Object value) async {
-
     List<List<Object?>> result = [];
     var values = await GetActiveValues(sheetName) ?? List.empty();
     var idx = columnIndex - 1;
@@ -79,5 +99,24 @@ class GoogleSheetManager {
     }
 
     return result;
+  }
+
+  Future<int> GetIndex(String sheetName, int columnIndex, Object? value) async {
+    if (value == null) return -1;
+
+    var values = await GetActiveValues(sheetName) ?? List.empty();
+    var idx = columnIndex - 1;
+
+    for (int i = 0; i < values.length; i++) {
+      var row = values[i];
+
+      if (row.length > columnIndex) {
+        if (row[idx] == value) {
+          return i + 1;
+        }
+      }
+    }
+
+    return -1;
   }
 }
